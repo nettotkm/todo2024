c808644256b0addb03d7a39031c500a302f4657f
nettotkm <netto.mtx@gmail.com>
2024-06-06 21:45:07 -0300

    add delete button and layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,47 @@ import "./App.css";
 import ListTodo from "./components/ListTodo";
 import Todo from "./components/Todo";
 
-const App: React.FC = () => {
-  const [tasks, setTasks] = useState<string[]>([]);
+interface Task {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
-  const handleAddtodo = (task: string) => {
-    setTasks([...tasks, task]);
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [nextTaskId, SetNextTaskId] = useState<number>(1);
+
+  const handleAddtodo = (taskText: string) => {
+    const newTask: Task = {
+      id: nextTaskId,
+      text: taskText,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    SetNextTaskId(nextTaskId + 1);
+  };
+
+  const handleToggle = (taskId: number) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const handleDelete = (taskID: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskID);
+    setTasks(updatedTasks);
   };
 
   return (
     <>
       <div>
         <Todo onAddTask={handleAddtodo} />
-        <ListTodo tasks={tasks} />
+        <ListTodo
+          tasks={tasks}
+          handleToggle={handleToggle}
+          handleDelete={handleDelete}
+        />
       </div>
     </>
   );
--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,14 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 interface ListTodoProps {
-  tasks: string[];
+  tasks: { id: number; text: string; completed: boolean }[];
+  handleToggle: (taskId: number) => void;
+  handleDelete: (taskId: number) => void;
 }
 
-const ListTodo: React.FC<ListTodoProps> = ({ tasks }) => {
+const ListTodo: React.FC<ListTodoProps> = ({
+  tasks,
+  handleToggle,
+  handleDelete,
+}) => {
   return (
-    <ul>
-      {tasks.map((task, index) => (
-        <li key={index}>{task}</li>
+    <div className="task-list">
+      {tasks.map((task) => (
+        <div
+          key={task.id}
+          className={`task-card ${task.completed} ? 'completed'': ''`}
+        >
+          <input
+            type="checkbox"
+            checked={task.completed}
+            onChange={() => handleToggle(task.id)}
+          />
+          <span
+            className="task-name"
+            style={{ textDecoration: task.completed ? "line-through" : "none" }}
+          >
+            {task.text}
+          </span>
+          <button
+            onClick={() => handleDelete(task.id)}
+            className="delete-button"
+          >
+            {" "}
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
 
